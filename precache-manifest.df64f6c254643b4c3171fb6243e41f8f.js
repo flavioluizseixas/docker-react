4f42d387ffe3cbd6e0d6bcdbc8994fd1a572641d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-26 13:50:02 +0000

    Deploy flavioluizseixas/docker-react to github.com/flavioluizseixas/docker-react.git:gh-pages

---
+++ precache-manifest.df64f6c254643b4c3171fb6243e41f8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e0cd190ff52a2f873808cea2750026f",
+    "url": "/docker-react/index.html"
+  },
+  {
+    "revision": "de03c5bd7897cab1dc9a",
+    "url": "/docker-react/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e06f21e88b7c1ba24166",
+    "url": "/docker-react/static/js/2.3f16b3fd.chunk.js"
+  },
+  {
+    "revision": "de03c5bd7897cab1dc9a",
+    "url": "/docker-react/static/js/main.e1192cea.chunk.js"
+  },
+  {
+    "revision": "3c7fad4ea2201cfc56aa",
+    "url": "/docker-react/static/js/runtime-main.26c7ed68.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/docker-react/static/media/logo.25bf045c.svg"
+  }
+]);
